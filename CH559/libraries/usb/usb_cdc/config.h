f75711c56c7ef126af8411be3026d6c932e44989
wagiminator <wagiminator@web.de>
2023-09-01 19:22:37 +0200

    Added new libraries

---
+++ CH559/libraries/usb/usb_cdc/config.h
@@ -0,0 +1,22 @@
+// ===================================================================================
+// User configurations
+// ===================================================================================
+
+#pragma once
+
+// Pin definitions
+#define PIN_LED             P25       // pin connected to LED
+
+// USB device descriptor
+#define USB_VENDOR_ID       0x16C0    // VID (shared www.voti.nl)
+#define USB_PRODUCT_ID      0x27DD    // PID (shared CDC-ACM)
+#define USB_DEVICE_VERSION  0x0100    // v1.0 (BCD-format)
+
+// USB configuration descriptor
+#define USB_MAX_POWER_mA    50        // max power in mA 
+
+// USB descriptor strings
+#define MANUFACTURER_STR    'w','a','g','i','m','i','n','a','t','o','r'
+#define PRODUCT_STR         'C','H','5','5','9',' ','D','e','v','B','o','a','r','d'
+#define SERIAL_STR          'C','H','5','5','9','C','D','C'
+#define INTERFACE_STR       'C','D','C','-','S','e','r','i','a','l'
---
+++ CH559/libraries/usb/usb_cdc/usb_handler.h
@@ -0,0 +1,43 @@
+// ===================================================================================
+// USB Handler for CH559                                                      * v1.0 *
+// ===================================================================================
+
+#pragma once
+#include <stdint.h>
+#include "usb_descr.h"
+
+// ===================================================================================
+// Variables
+// ===================================================================================
+#define USB_SetupBuf     ((PUSB_SETUP_REQ)EP0_buffer)
+extern volatile uint8_t  USB_SetupReq, USB_SetupTyp;
+extern volatile uint16_t USB_SetupLen;
+
+// ===================================================================================
+// Custom External USB Handler Functions
+// ===================================================================================
+uint8_t CDC_control(void);
+void CDC_EP_init(void);
+void CDC_EP0_OUT(void);
+void CDC_EP2_IN(void);
+void CDC_EP2_OUT(void);
+
+// ===================================================================================
+// USB Handler Defines
+// ===================================================================================
+// Custom USB handler functions
+#define USB_INIT_endpoints  CDC_EP_init       // custom USB EP init handler
+#define USB_CTRL_NS_handler CDC_control       // handle custom non-standard requests
+
+// Endpoint callback functions
+#define EP0_SETUP_callback  USB_EP0_SETUP
+#define EP0_IN_callback     USB_EP0_IN
+#define EP0_OUT_callback    CDC_EP0_OUT
+#define EP2_IN_callback     CDC_EP2_IN
+#define EP2_OUT_callback    CDC_EP2_OUT
+
+// ===================================================================================
+// Functions
+// ===================================================================================
+void USB_interrupt(void);
+void USB_init(void);
