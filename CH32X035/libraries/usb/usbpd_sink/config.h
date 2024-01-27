cee22f2c29a9a228a4f5c88b15ad274e4970c93d
wagiminator <wagiminator@web.de>
2024-01-27 19:31:15 +0100

    Some updates

---
+++ CH32X035/libraries/usb/usbpd_sink/config.h
@@ -0,0 +1,11 @@
+// ===================================================================================
+// User configurations
+// ===================================================================================
+
+#pragma once
+
+// Pin definitions
+#define PIN_LED             PB1       // pin connected to LED
+
+// MCU supply voltage
+#define USB_VDD             0         // 0: 3.3V, 1: 5V
