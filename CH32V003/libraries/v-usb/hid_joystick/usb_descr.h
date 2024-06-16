5971ef842955286ef51c7414580a88a2e7bcb1e7
wagiminator <wagiminator@web.de>
2024-06-16 19:40:37 +0200

    Some updates

--- CH32V003/libraries/v-usb/hid_joystick/usb_descr.h
+++ CH32V003/libraries/v-usb/hid_joystick/usb_descr.h
@@ -47,36 +47,38 @@ extern "C" {
 // HID Report Descriptor
 // ===================================================================================
 static const uint8_t ReportDescr[] = {
-	0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-	0x09, 0x04,                    // USAGE (Joystick)
-	0xa1, 0x01,                    // COLLECTION (Application)
+  0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+  0x09, 0x04,                    // USAGE (Joystick)
+  0xa1, 0x01,                    // COLLECTION (Application)
 
-	0x09, 0x01,                    //   USAGE (Pointer)
-	0xa1, 0x00,                    //   COLLECTION (Physical)
-	0x09, 0x30,                    //     USAGE (X)
-	0x09, 0x31,                    //     USAGE (Y)
+  // Joystick position
+  0x09, 0x01,                    //   USAGE (Pointer)
+  0xa1, 0x00,                    //   COLLECTION (Physical)
+  0x09, 0x30,                    //     USAGE (X)
+  0x09, 0x31,                    //     USAGE (Y)
   0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
   0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
-	0x75, 0x08,                    //     REPORT_SIZE (8)
-	0x95, 0x02,                    //     REPORT_COUNT (2)
-	0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-	0xc0,                          //   END_COLLECTION
+  0x75, 0x08,                    //     REPORT_SIZE (8)
+  0x95, 0x02,                    //     REPORT_COUNT (2)
+  0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+  0xc0,                          //   END_COLLECTION
 
-	0x05, 0x09,                    //   USAGE_PAGE (Button)
-	0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
-	0x29, USB_BUTTONS,             //   USAGE_MAXIMUM (number of buttons)
-	0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-	0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
-	0x75, 0x01,                    //   REPORT_SIZE (1)
-	0x95, USB_BUTTONS,             //   REPORT_COUNT (number of buttons)
-	0x81, 0x02,                    //   INPUT (Data,Var,Abs)
-	#if USB_BUTTONS < 8
-	0x75, 0x01,                    //   REPORT_SIZE (1)
-	0x95, 0x08 - USB_BUTTONS,      //   REPORT_COUNT (number of dummy bits)
-	0x81, 0x03,                    //   INPUT (Const,Var,Abs)
-	#endif
+  // Joystick buttons
+  0x05, 0x09,                    //   USAGE_PAGE (Button)
+  0x19, 0x01,                    //   USAGE_MINIMUM (Button 1)
+  0x29, USB_BUTTONS,             //   USAGE_MAXIMUM (number of buttons)
+  0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
+  0x25, 0x01,                    //   LOGICAL_MAXIMUM (1)
+  0x75, 0x01,                    //   REPORT_SIZE (1)
+  0x95, USB_BUTTONS,             //   REPORT_COUNT (number of buttons)
+  0x81, 0x02,                    //   INPUT (Data,Var,Abs)
+  #if USB_BUTTONS < 8
+  0x75, 0x01,                    //   REPORT_SIZE (1)
+  0x95, 0x08 - USB_BUTTONS,      //   REPORT_COUNT (number of dummy bits)
+  0x81, 0x03,                    //   INPUT (Const,Var,Abs)
+  #endif
 
-	0xc0                           // END_COLLECTION (application)
+  0xc0                           // END_COLLECTION (application)
 };
 
 // ===================================================================================
